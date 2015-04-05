eb83e0c399bc3a7412d567856f1e0c9c0469ddf7
James Gifford <jg.webdeveloper@gmail.com>
2015-04-05 01:24:21 -0700

    Adding skills and work sections plus general styling updates

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -26,16 +26,46 @@
 			<a href="#contact" class="glyphicon glyphicon-menu-down"></a>
 		</header>
 
+		<section id="about">
+			<p>Hello, my name is James Gifford and I am a programmer of web and mobile applications using a wide range of <a href="#skills">modern technologies</a>. My approach to writing code is the same as with <a href=""> my other passions</a> in life: seek out new challenges and never stop learning.</p>
+		</section>
+
 		<section id="skills">
+			<h2>My Skills</h2>
+			<p>Some of the technologies I work with</p>
+			<ul>
+				<li class="php"><a href="http://php.net" target="_blank" title="PHP">PHP</a></li>
+				<li class="laravel"><a href="http://laravel.com" target="_blank" title="Laravel">Laravel</a></li>
+				<li class="codeigniter"><a href="http://codeigniter.com" target="_blank" title="CodeIgniter">CodeIgniter</a></li>
+				<li class="cakephp"><a href="http://cakephp.org" target="_blank" title="CakePHP">CakePHP</a></li>
+				<li class="wordpress"><a href="http://wordpress.org" target="_blank" title="WordPress">WordPress</a></li>
+				<li class="python"><a href="http://python.org" target="_blank" title="Python">Python</a></li>
+				<li class="html"><a href="http://www.w3schools.com/html/" target="_blank" title="HTML">HTML</a></li>
+				<li class="css"><a href="http://www.w3schools.com/css/" target="_blank" title="CSS">CSS</a></li>
+				<li class="javascript"><a href="http://www.w3schools.com/js/" target="_blank" title="JavaScript">JavaScript</a></li>
+				<li class="jquery"><a href="http://jquery.com" target="_blank" title="jQuery">jQuery</a></li>
+				<li class="angular"><a href="http://angularjs.org" target="_blank" title="Angular">Angular</a></li>
+				<li class="react"><a href="https://facebook.github.io/react/" target="_blank" title="React">React</a></li>
+				<li class="swift"><a href="https://developer.apple.com/swift/" target="_blank" title="Swift">Swift</a></li>
+				<li class="phonegap"><a href="http://phonegap.com" target="_blank" title="PhoneGap">PhoneGap</a></li>
+				<li class="ionic"><a href="http://ionicframework.com" target="_blank" title="Ionic">Ionic</a></li>
+				<li class="sass"><a href="http://sass-lang.com" target="_blank" title="Sass">Sass</a></li>
+				<li class="bootstrap"><a href="http://getbootstrap.com" target="_blank" title="Bootstrap">Bootstrap</a></li>
+				<li class="git"><a href="http://git-scm.com" target="_blank" title="Git">Git</a></li>
+			</ul>
 		</section>
 
 		<section id="work">
+			<h2>My Work</h2>
+			<p>Some of the projects I've been writing code for recently</p>
+			<ul>
+				<li class="bandrobot"><a href="http://bandrobot.com" target="_blank" title="Band Robot"></a><span>Band Robot</span></li>
+				<li class="mightynurse"><a href="http://mightynurse.com" target="_blank" title="Mighty Nurse"></a><span>Mighty Nurse</span></li>
+				<li class="traveloregon"><a href="http://traveloregon.com" target="_blank" title="Travel Oregon"></a><span>Travel Oregon</span></li>
+			</ul>
 		</section>
 
 		<section id="fun">
-		</section>
-
-		<section id="about">
 		</section>
 
 		<section id="social">
@@ -43,6 +73,7 @@
 
 		<section id="contact">
 			<h2>Contact Me</h2>
+			<p>Let me know if you'd like to discuss a project or just shoot the breeze</p>
 			{!! Form::open(['url' => 'contact', 'method' => 'post']) !!}
 
 				{!! Form::label('name', 'Name:') !!}
@@ -65,6 +96,7 @@
 		</footer>
 
 		<script src="{{ asset('assets/js/jquery.min.js') }}"></script>
+		<script src="{{ asset('assets/bootstrap/js/bootstrap.min.js') }}"></script>
 		<script src="{{ asset('assets/js/app.js') }}"></script>
 	</body>
 </html>
